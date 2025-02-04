df0a308e636c1235c7188d85d194926def343612
Kyle Harrington <czi@kyleharrington.com>
2025-02-03 21:45:26 -0500

    Site config

---
+++ .atrium/site_config.py
@@ -0,0 +1,12 @@
+"""Site configuration for the UV scripts collection."""
+
+SITE_CONFIG = {
+    "project_name": "atrium.cellcanvas.org",
+    "project_description": "a collection of executable scripts for studying cellular architecture",
+    "base_url": "https://atrium.cellcanvas.org",
+    "github_repo": "https://github.com/cellcanvas/cellcanvas",
+    "author": {
+        "name": "CellCanvas Team",
+        "email": "cellcanvas@kyleharrington.com"
+    }
+}
